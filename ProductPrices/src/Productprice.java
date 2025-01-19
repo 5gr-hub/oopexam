cb227028c4a46dcca86eb638215a34869f71aa10
5gr-hub <victoraaron696@gmail.com>
2025-01-19 13:47:44 +0300

    Add files via upload

---
+++ ProductPrices/src/Productprice.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Productprice {
+    public static void main(String[] args) {
+        double[] productprice = 
+        {3000, 10000, 2000, 5000, 8000};
+        for (int i = 0; i< productprice.length; i++) {
+            System.out.println("Price of product" + (i + i) + ":" + productprice[i]);
+        }
+    }
+}
---
+++ ProductPrices/src/productprices/ProductPrices.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package productprices;
+
+/**
+ *
+ * @author user
+ */
+public class ProductPrices {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
